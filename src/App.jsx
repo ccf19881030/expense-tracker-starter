5609b0d7e12c7a48bb656bb177adcca7fd08e9c3
ccf19881030.github.io <ccf19881030@users.noreply.github.com>
2026-04-29 23:36:16 +0800

    Add delete transaction functionality

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,10 @@ function App() {
     setTransactions([...transactions, transaction]);
   };
 
+  const handleDeleteTransaction = (id) => {
+    setTransactions(transactions.filter(t => t.id !== id));
+  };
+
   return (
     <div className="app">
       <h1>Finance Tracker</h1>
@@ -27,7 +31,7 @@ function App() {
 
       <Summary transactions={transactions} />
       <TransactionForm onAdd={handleAddTransaction} />
-      <TransactionList transactions={transactions} />
+      <TransactionList transactions={transactions} onDelete={handleDeleteTransaction} />
     </div>
   );
 }
